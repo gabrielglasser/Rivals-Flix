a87f071be182d0bcdb6da6e42f09d2efa5414093
gabrielglasser <sga91000@gmail.com>
2024-12-30 00:14:39 -0300

    Criando a Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 function Home() {
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("/movie/now_playing", {
+        params: {
+          api_key: "1166f2f22cf014d88a69aa246561c7a0",
+          language: "pt-BR",
+          page: 1,
+        }
+      });
+
+      
+
+    }
+
+    loadFilmes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
