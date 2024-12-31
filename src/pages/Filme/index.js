d66b954650c43ca900b902bdb6c03ff7df584250
gabrielglasser <sga91000@gmail.com>
2024-12-31 00:13:19 -0300

    Personalizando o alerta

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import "./filme-info.css";
+import { toast } from "react-toastify";
 
 function Filme() {
   const { id } = useParams();
@@ -43,13 +44,13 @@ function Filme() {
     const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
 
     if (hasFilme) {
-      alert("Esse filme já está na sua lista!");
+      toast.warn("Você já possui esse filme salvo!");
       return;
     }
 
     filmesSalvos.push(filme);
     localStorage.setItem("@Rivalsflix", JSON.stringify(filmesSalvos));
-    alert("Filme salvo com sucesso!");
+    toast.success("Filme salvo com sucesso!");
   }
 
   if (loading) {
