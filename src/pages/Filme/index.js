1c8380366744c48070baea54355e5511131c072b
gabrielglasser <sga91000@gmail.com>
2024-12-30 23:16:03 -0300

    Salvando Filmes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -35,6 +35,23 @@ function Filme() {
     };
   }, [id, navigate]);
 
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@Rivalsflix");
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
+
+    if (hasFilme) {
+      alert("Esse filme já está na sua lista!");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@Rivalsflix", JSON.stringify(filmesSalvos));
+    alert("Filme salvo com sucesso!");
+  }
+
   if (loading) {
     return (
       <div className="filme-info">
@@ -57,7 +74,7 @@ function Filme() {
       <strong>Nota: {filme.vote_average} / 10</strong>
 
       <div className="area-botoes">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
           <a
             target="blank"
